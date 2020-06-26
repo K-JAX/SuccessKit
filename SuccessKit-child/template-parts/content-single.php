ca305b1a7168f669523f226acad79d6daaa4e604
Kevin Garubba <kevingarubba@gmail.com>
2020-06-26 15:20:31 -0400

    Removing extra space in header. Removing h4 title 'Blog Posts' shown in blog post header.

--- SuccessKit-child/template-parts/content-single.php
+++ SuccessKit-child/template-parts/content-single.php
@@ -21,7 +21,7 @@
 <div class="row">
 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
 <div class="process-text" align="center">
-<h4>Blog Post</h4>
+<!-- <h4>Blog Post</h4> -->
 <h1> <?php the_title();?></h1>
 
  
