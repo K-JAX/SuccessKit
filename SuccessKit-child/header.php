e96832315a3467c9e29087d16d79f2419c213c1b
Kevin Garubba <kevingarubba@gmail.com>
2020-06-27 16:07:00 -0400

    Adding two templates and setting up conditional to safely remove hard-coded functions one at a time.

--- SuccessKit-child/header.php
+++ SuccessKit-child/header.php
@@ -11,64 +11,68 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/owl.carousel.min.css" type="text/css"/>
-    
 
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/owl.theme.default.min.css" type="text/css"/>
+  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/owl.carousel.min.css" type="text/css"/>
+
+
+  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/assets/css/owl.theme.default.min.css" type="text/css"/>
 
 
   <link href="https://fonts.googleapis.com/css?family=Alegreya:400,400i,500,700,800|Roboto+Mono:100,300,400,400i,500,700&display=swap" rel="stylesheet">
-	
 
 
-    <?php wp_head(); ?>
+
+    <?php wp_head();?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class();?>>
 <div class="header-bg">
   <header class="header--fixed">
-  
-   <?php 
-   if(get_theme_mod( 'educational_top_header_enable' ) == '1'){
-    get_template_part( 'header-parts/header','top' );
-  }
 
-  get_template_part( 'header-parts/header','bottom' );
-  
-  
-  ?>
- 
+   <?php
+if (get_theme_mod('educational_top_header_enable') == '1') {
+    get_template_part('header-parts/header', 'top');
+}
+
+get_template_part('header-parts/header', 'bottom');
+
+?>
+
 </header>
 </div>
 <header class="header--fixed placeholder-only"></header>
 
-<?php if( is_home() || (!is_front_page()) || !(is_page_template('template-home.php'))):?>
+<?php if (is_home() || (!is_front_page()) || !(is_page_template('template-home.php'))): ?>
 <!-- Breadcrumb -->
+
+<?php 
+// starting to exclude individual templates from this
+// system one at a time with the following condition
+ if (!is_page_template('page-templates/page-hero.php') && !is_page_template('page-templates/page-headline.php')):
+?>
 <div class="top-block">
-<?php $ptitle = get_the_title(); ?>
-  <div class="top-title" style="background: url(<?php if(has_header_image()):echo esc_url(get_header_image());endif;?>);">
-   <?php if(is_home()): ?>
-    <h2><?php bloginfo('name'); ?></h2>
-    
-    <?php elseif( $ptitle == "Pricing" ): ?>
+<?php $ptitle = get_the_title();?>
+  <div class="top-title" style="background: url(<?php if (has_header_image()): echo esc_url(get_header_image());endif;?>);">
+   <?php if (is_home()): ?>
+    <h2><?php bloginfo('name');?></h2>
+
+    <?php elseif ($ptitle == "Pricing"): ?>
     <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
      </div>
-     <?php elseif( $ptitle == "About us" ): ?>
-     
+     <?php elseif ($ptitle == "About us"): ?>
+
        <div class="about-us-text header-bg">
        <div class="container">
     <div class="banner-size">
@@ -77,7 +81,7 @@
                 <div class="slider-text abouts-text-h">
                     <h1>About us</h1>
                     <p>We are a small team of sales, marketing, and content professionals devoted to enabling all companies to leverage their client’s success in the form of Case Studies.</p>
-                   
+
                 </div>
             </div>
 
@@ -92,32 +96,21 @@
 </div>
 </div>
        </div>
-     <?php elseif( $ptitle == "Our Work" ): ?>
+     <?php elseif ($ptitle == "Our Work"): ?>
       <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
      </div>
 
-     <?php elseif( $ptitle == "Our Own Case Studies" ): ?>
-      <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
-     </div>
 
-<?php elseif( $ptitle == "Our Process" ): ?>
-     
+
+<?php elseif ($ptitle == "Our Process"): ?>
+
        <div class="about-us-text header-bg">
        <div class="container">
     <div class="banner-size">
@@ -126,7 +119,7 @@
                 <div class="slider-text abouts-text-h">
                     <h1>Our process & offering</h1>
                     <p>SuccessKit handles your Case Studies from start to finish. We have refined our process by creating hundreds of unique Case Studies, and we have seen it all!</p>
-                   
+
                 </div>
             </div>
 
@@ -141,82 +134,76 @@
 </div>
 </div>
 
-<?php elseif( $ptitle == "Blog" ): ?>
+<?php elseif ($ptitle == "Blog"): ?>
       <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
-     </div>
-	 
-	 
-	 <?php elseif( $ptitle == "Article" ): ?>
-      <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
-     </div>
-     
-     <?php elseif( $ptitle == "Book a Demo" ): ?>
-      <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
-     </div>
-     
-     <?php elseif( $ptitle == "Privacy Policy" ): ?>
-      <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
-     </div>
-     
-      <?php elseif( $ptitle == "Request Info" ): ?>
-      <div class="pricing">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
      </div>
 
-     <?php elseif( $ptitle == "Contact Us" ): ?>
+
+	 <?php elseif ($ptitle == "Article"): ?>
+      <div class="pricing">
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
+     </div>
+
+     <?php elseif ($ptitle == "Book a Demo"): ?>
+      <div class="pricing">
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
+     </div>
+
+     <?php elseif ($ptitle == "Privacy Policy"): ?>
+      <div class="pricing">
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
+     </div>
+
+      <?php elseif ($ptitle == "Request Info"): ?>
+      <div class="pricing">
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
+     </div>
+
+     <?php elseif ($ptitle == "Contact Us"): ?>
       <div class="pricing book-demo">
-      <h2><?php if ( is_archive() ) {
-        the_archive_title( '<h2>', '</h2>' );
-      }
-      else{
-        echo '<h2>';
-        echo esc_html( get_the_title() );
-        echo '</h2>';
-      } ?></h2>
+      <h2><?php if (is_archive()) {
+    the_archive_title('<h2>', '</h2>');
+} else {
+    echo '<h2>';
+    echo esc_html(get_the_title());
+    echo '</h2>';
+}?></h2>
      </div>
-
-    <?php endif; ?>
+<?php endif; ?>
+    <?php endif;?>
   </div>
- 
- 
+
+
 </div>
   <?php endif;?>
---
+++ SuccessKit-child/page-templates/page-hero.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Hero Page
+ */
+get_header(); ?>
+
+
+
+<div class="top-block">
+    <div class="top-title">
+        <?php while (have_posts()): the_post();?>
+        <div class="pricing">
+            <h2><?php the_title();?></h2>
+        </div>
+    </div>
+
+        <?php the_content();?>
+    <?php endwhile;?>
+</div>
+
+<?php 
+get_footer();
+?>
