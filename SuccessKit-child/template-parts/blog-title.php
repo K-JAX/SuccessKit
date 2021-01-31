fc9b81c5d32668a36000d52158099b76e018fd0a
Kevin Garubba <kevingarubba@gmail.com>
2021-01-31 08:02:10 -0500

    Adding container to the blog title template part so that it doesn't touch edges.

--- SuccessKit-child/template-parts/blog-title.php
+++ SuccessKit-child/template-parts/blog-title.php
@@ -1,13 +1,13 @@
 <?php
-/**
- * Template part for displaying blog title
- */
+    /**
+     * Template part for displaying blog title
+     */
 ?>
 <div class="top-block">
-    <div class="top-title">
-        <div class="headline-title py-5 text-center">
-            <h2 style="text-transform: initial;"><?php echo $args['title']; ?></h2>
-            <div class="sans-serif h5 my-4"><?php echo $args['desc']; ?></div>
-        </div>
-     </div>
+	<div class="top-title">
+		<div class="container headline-title py-5 text-center">
+			<h2 style="text-transform: initial;"><?php echo $args['title']; ?></h2>
+			<div class="sans-serif h5 my-4"><?php echo $args['desc']; ?></div>
+		</div>
+	</div>
 </div>
