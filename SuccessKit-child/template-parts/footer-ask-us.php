8054769b12e6ba6b8f38cd395277fdb981d67f2b
Kevin Garubba <kevingarubba@gmail.com>
2021-06-15 23:10:00 -0400

    Updating fonts and form settinsg in the footer. Adding template part for the Ask us anything section.

---
+++ SuccessKit-child/template-parts/footer-ask-us.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template part for displaying the "Ask us anything" section
+ */
+
+ ?>
+
+<div class="ask-us bg-blue">
+	<div class="container py-5 mb-0">
+		<div class="row">
+			<div class="col-md-6 col-12">
+				<h2 class="text-white text-initial display-3">Ask us anything</h2>
+			</div>
+			<div class="col-md-6 col-12">
+				<p class="text-white h4 mb-5">Have a question? Reach out to us directly.</p>
+				<a href="mailto:julian@successkit.io" target="_blank" class="theme-btn d-inline-block h4">Contact us</a>
+			</div>
+		</div>
+	</div>
+</div>
